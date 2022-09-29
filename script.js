b1aa95499c513ec7c9d30547a0be630b8c9321f4
teejay128 <104542152+Teejay128@users.noreply.github.com>
2022-09-29 18:00:26 +0100

    added functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+// Get modal element
+let modal = document.querySelector('.modal')
+
+// Get open modal button
+let openBtn = document.querySelector('.openBtn')
+
+// Get close modal button
+let closeBtn = document.querySelector('.closeBtn')
+
+
+// Listen for open click
+openBtn.addEventListener('click', openModal)
+
+// Listen for close click
+closeBtn.addEventListener('click', closeModal)
+
+// Listen for outside click
+window.addEventListener('click', outsideClick)
+
+// Function to open modal
+function openModal() {
+    modal.style.display = 'block'
+    document.querySelector('.opaque').style.opacity = '0.2'
+}
+
+function closeModal() {
+    modal.style.display = 'none'
+}
+
+function outsideClick(e) {
+    if(e.target == modal){
+    modal.style.display = 'none'
+    }
+}
