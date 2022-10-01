518f6532278d95acb84519e285e613ab50514139
teejay128 <104542152+Teejay128@users.noreply.github.com>
2022-10-01 02:19:54 +0100

    I planned to make the content of the modal dynamic, but....

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Get modal element
-let modal = document.querySelector('.modal')
+let container = document.querySelector('.container')
+let openBtn = document.querySelector('form');
+let myName = document.querySelector('#name');
+let favColor = document.querySelector('#favColor');
+let closeBtn = document.querySelector('#closeBtn');
+let title = document.querySelector('.title')
+let modal =  document.querySelector('.modal');
 
-// Get open modal button
-let openBtn = document.querySelector('#submit')
-
-// Get close modal button
-let closeBtn = document.querySelector('#closeBtn')
 
 
-// Listen for open click
-openBtn.addEventListener('click', openModal)
+openBtn.addEventListener('submit', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
 
-// Listen for close click
-closeBtn.addEventListener('click', closeModal)
-
-// Listen for outside click
-window.addEventListener('click', outsideClick)
-
-// Function to open modal
 function openModal() {
-    modal.style.display = 'block'
-    document.querySelector('.opaque').style.opacity = '0.2'
+    let color = favColor.value;
+    modal.style.display = 'block';
+    modal.style.backgroundColor = color;
+    modal.style.color = color;
+    title.innerText = `Dear ${myName.value},`
+    container.style.opacity = '0.2';
 }
 
 function closeModal() {
-    modal.style.display = 'none'
+    modal.style.display = 'none';
+    container.style.backgroundColor = '#fff';
+    container.style.opacity = '1';
 }
 
 function outsideClick(e) {
     if(e.target == modal){
-    modal.style.display = 'none'
+    modal.style.display = 'none';
+    container.style.backgroundColor = '#fff';
+    container.style.opacity = '1';
     }
 }
